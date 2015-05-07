ddbc3a2c0cc1327b2f713890bcc00b43187a2bc3
Baris Taze <baris@uber.com>
2015-05-07 13:28:50 -0700

    clean-up

--- InstagramFeed/PhotoTableHeaderCell.swift
+++ InstagramFeed/PhotoTableHeaderCell.swift
@@ -17,20 +17,6 @@ class PhotoTableHeaderCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        
-        /*
-        self.userPhotoView.frame = CGRect(x:2, y:2, width:self.frame.height-4, height:self.frame.height-4)
-        self.userNameView.frame = CGRect(x:self.userPhotoView.frame.width + 4, y:2, width:self.frame.width - self.userPhotoView.frame.width - 6, height:self.frame.height-4)
-        
-        self.userPhotoView.layer.cornerRadius = self.userPhotoView.frame.size.width / 2
-        self.userPhotoView.clipsToBounds = true
-        */
-        
-        self.userPhotoView.frame = CGRect(x:0, y:5, width:50, height:50)
-        self.userNameView.frame = CGRect(x:52, y:5, width:268, height:50)
-        
-        self.userPhotoView.layer.cornerRadius = 25
-        self.userPhotoView.clipsToBounds = true
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
