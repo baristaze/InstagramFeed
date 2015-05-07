1a78f8081f7fb74651f31235ce17fb03ed16950a
Baris Taze <baris@uber.com>
2015-05-06 19:44:32 -0700

    creating photos view contoller

---
+++ InstagramFeed/PhotosViewController.swift
@@ -0,0 +1,45 @@
+//
+//  PhotosViewController.swift
+//  InstagramFeed
+//
+//  Created by Baris Taze on 5/6/15.
+//  Copyright (c) 2015 Baris Taze. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+    var photos:NSArray!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        var clientId = "d654efc721c344908eb0ba443d52ac7f"
+        var url = NSURL(string: "https://api.instagram.com/v1/media/popular?client_id=\(clientId)")!
+        var request = NSURLRequest(URL: url)
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+            var responseDictionary = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as! NSDictionary
+            self.photos = responseDictionary["data"] as! NSArray
+            //self.tableView.reloadData()
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
