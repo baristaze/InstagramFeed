61b1a5b111984959376f0df75e7ba93dd039b60c
Baris Taze <baris@uber.com>
2015-05-07 09:35:59 -0700

    Adding pull and refresh feature

--- InstagramFeed/PhotosViewController.swift
+++ InstagramFeed/PhotosViewController.swift
@@ -10,31 +10,55 @@ import UIKit
 
 class PhotosViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    var photos:NSArray!
+    var photos:NSMutableArray!
+    var refreshControl:UIRefreshControl!
+    let clientId = "d654efc721c344908eb0ba443d52ac7f"
     
     @IBOutlet weak var tableView: UITableView!
+    
+    typealias onInstagramData = (NSArray) -> (Void)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.refreshControl = UIRefreshControl()
+        self.refreshControl.addTarget(self, action: "onRefresh", forControlEvents: UIControlEvents.ValueChanged)
+        self.tableView.insertSubview(self.refreshControl, atIndex: 0)
+            
         self.tableView.rowHeight = 320
         self.tableView.dataSource = self
         self.tableView.delegate = self
         
-        // Do any additional setup after loading the view.
-        var clientId = "d654efc721c344908eb0ba443d52ac7f"
-        var url = NSURL(string: "https://api.instagram.com/v1/media/popular?client_id=\(clientId)")!
-        var request = NSURLRequest(URL: url)
-        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
-            var responseDictionary = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as! NSDictionary
-            self.photos = responseDictionary["data"] as! NSArray
-            self.tableView.reloadData()
-        }
+        self.photos = NSMutableArray()
+        
+        self.queryInstagramWithCallback({(data:NSArray)->(Void) in
+            self.photos.addObjectsFromArray(data as [AnyObject])
+            self.tableView.reloadData()})
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func queryInstagramWithCallback(callback:onInstagramData){
+        
+        // Do any additional setup after loading the view.
+        let url = NSURL(string: "https://api.instagram.com/v1/media/popular?client_id=\(clientId)")!
+        var request = NSURLRequest(URL: url)
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+            var responseDictionary = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as! NSDictionary
+            var data = responseDictionary["data"] as! NSArray
+            callback(data)
+        }
+    }
+    
+    func onRefresh() {
+        self.queryInstagramWithCallback({(data:NSArray)->(Void) in
+            self.photos.addObjectsFromArray(data as [AnyObject])
+            self.tableView.reloadData()
+            self.refreshControl.endRefreshing()
+        })
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
