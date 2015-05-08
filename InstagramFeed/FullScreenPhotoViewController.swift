7294b558c72d7dab80e0273d849f7ea1b05ff6dc
Baris Taze <baris@uber.com>
2015-05-08 13:01:43 -0700

    moving close button out of scrollview. better close button.

--- InstagramFeed/FullScreenPhotoViewController.swift
+++ InstagramFeed/FullScreenPhotoViewController.swift
@@ -22,8 +22,8 @@ class FullScreenPhotoViewController: UIViewController, UIScrollViewDelegate {
         self.scrollView.delegate = self
         
         let layer = self.closeButton.layer
-        layer.backgroundColor = UIColor(red:0.3, green: 0.3, blue:0.3, alpha:0.8).CGColor
-        layer.borderColor = UIColor.darkGrayColor().CGColor
+        layer.backgroundColor = UIColor(red:0.3, green: 0.3, blue:0.3, alpha:0.6).CGColor
+        layer.borderColor = UIColor.whiteColor().CGColor
         layer.borderWidth = 1.0
         
         let url = NSURL(string: photoUrl!)
